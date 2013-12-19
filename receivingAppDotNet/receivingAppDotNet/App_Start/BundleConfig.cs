e97b4d322c25022a7579c4a0b8253ffcd3ddbb07
jaredfaris <jfaris@gmail.com>
2013-12-18 22:52:23 -0500

    Added in some inline and on-page javascript functions.

--- receivingAppDotNet/receivingAppDotNet/App_Start/BundleConfig.cs
+++ receivingAppDotNet/receivingAppDotNet/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace receivingAppDotNet
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js"
+            ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -25,6 +27,20 @@ namespace receivingAppDotNet
                       "~/Content/bootstrap.css",
                       "~/Content/offcanvas.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                          "~/Content/themes/base/jquery.ui.core.css",
+                          "~/Content/themes/base/jquery.ui.resizable.css",
+                          "~/Content/themes/base/jquery.ui.selectable.css",
+                          "~/Content/themes/base/jquery.ui.accordion.css",
+                          "~/Content/themes/base/jquery.ui.autocomplete.css",
+                          "~/Content/themes/base/jquery.ui.button.css",
+                          "~/Content/themes/base/jquery.ui.dialog.css",
+                          "~/Content/themes/base/jquery.ui.slider.css",
+                          "~/Content/themes/base/jquery.ui.tabs.css",
+                          "~/Content/themes/base/jquery.ui.datepicker.css",
+                          "~/Content/themes/base/jquery.ui.progressbar.css",
+                          "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
