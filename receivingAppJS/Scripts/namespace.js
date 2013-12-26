523c540d11c0b9a85daceeac09592e86fb2d8fd0
Jared Faris <jfaris@gmail.com>
2013-12-26 12:20:39 -0500

    DRYd up deletions

---
+++ receivingAppJS/Scripts/namespace.js
@@ -0,0 +1,30 @@
+window.receivingApp = {
+    part: {},
+    vendor: {},
+    utility: {}
+};
+
+// shorthand
+var utility = window.receivingApp.utility;
+
+utility.deletePopup = {
+
+   open: function (title, deleteFunction) {
+        $('<div><h4>Please confirm this delete.</h4></div>').dialog({
+            resizable: false,
+            height: 200,
+            modal: true,
+            title: title,
+            buttons: {
+                Confirm: function() {
+                    deleteFunction();
+
+                    $(this).dialog("close");
+                },
+                Cancel: function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    }
+};
