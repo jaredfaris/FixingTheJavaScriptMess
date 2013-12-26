4893689315f60e2c96585bbf48624af88a22282c
Jared Faris <jfaris@gmail.com>
2013-12-26 14:27:30 -0500

    DRYd up Part.html too

--- receivingAppJS/Scripts/namespace.js
+++ receivingAppJS/Scripts/namespace.js
@@ -49,9 +49,36 @@ window.receivingApp.vendor = function () {
 
             window.receivingApp.utility.deletePopup.open("Delete this vendor?", deleteAjax);
         });
-    }
+    };
 
     return {
         initializeDeleteLink: initializeDeleteLink
     };
-}
+};
+
+window.receivingApp.part = function () {
+    // initializes all of the delete links in the current parts list and wires up their AJAX post
+    var initializeDeleteLink = function() {
+        $('#currentParts').on('click', '.deletePartLink', function (event) {
+            event.preventDefault();
+
+            var partId = $(this).parents('tr').data('partid');
+
+            var deleteFunction = function () {
+                $.ajax({
+                    type: "POST",
+                    url: "/Part/Delete",
+                    data: { Id: partId },
+                    context: this
+                }).done(function () {
+                    $('#partsList').find('tr[data-partid="' + partId + '"]').remove();
+                });
+            }
+            window.receivingApp.utility.deletePopup.open("Delete this vendor?", deleteFunction);
+        });
+    };
+
+    return {
+        initializeDeleteLink: initializeDeleteLink
+    };
+};
