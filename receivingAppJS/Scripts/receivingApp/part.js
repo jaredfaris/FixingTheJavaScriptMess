d9ec7c27f725cda1b544dc07dd0f896fb9103525
Jared Faris <jfaris@gmail.com>
2013-12-26 15:06:18 -0500

    Removed almost all of the on-page JS

--- receivingAppJS/Scripts/receivingApp/part.js
+++ receivingAppJS/Scripts/receivingApp/part.js
@@ -47,14 +47,88 @@ window.receivingApp.part = function () {
 
                 $('#currentParts').html(markup);
             });
-    }
+    };
 
-    var toggleDiscontinuedParts = function () {
+    // wires up the Hide/Show Discontinued Parts link
+    var initializeToggleDiscontinuedLink = function () {
+        $('#toggleDiscontinuedParts').on('click', function (event) {
+            event.preventDefault();
 
-    }
+            var isShowing = $(this).hasClass('expanded');
+
+            if (!isShowing) {
+                // show the parts
+                $('#toggleDiscontinuedParts').text('Hide Discontinued Parts').addClass('expanded');
+                loadDiscontinuedParts();
+            } else {
+                // hide the parts
+                $('#toggleDiscontinuedParts').text('Show Discontinued Parts').removeClass('expanded');
+                $('#discontinuedParts').html('');
+            }
+        });
+    };
+
+    // loads/reloads the discontinued parts grid
+    var loadDiscontinuedParts = function () {
+        $.ajax({
+            url: '/Part/DiscontinuedParts',
+            type: 'GET',
+            context: this,
+            dataType: "json"
+        }).done(function (result) {
+                var markup = '<table class="table"><tr><th>Name</th><th>Weight</th><th></th></tr>';
+
+                _.each(result, function (item) {
+                    markup += "<tr><td>" + item.Name + "</td><td>" + item.Weight + "</td><td></td></tr>";
+                });
+
+                markup += "</table>";
+
+                $('#discontinuedParts').html(markup);
+            });
+    };
+
+    // loads/reloads the current parts grid
+    var loadCurrentParts = function () {
+        // load the current parts at page load
+        $.ajax({
+            url: '/Part/CurrentParts',
+            type: 'GET',
+            context: this,
+            dataType: "json"
+        }).done(function (result) {
+                var markup = '<table class="table" id="partsList"><tr><th>Name</th><th>Weight</th><th></th></tr>';
+
+                _.each(result, function (item) {
+                    markup += "<tr data-partid=\"" + item.Id + "\">" +
+                        "<input type=\"hidden\" class=\"partId\" value=\"" + item.Id + "\"/>" +
+                        "<td>" + item.Name + "</td>" +
+                        "<td>" + item.Weight + "</td>" +
+                        "<td><a class=\"deletePartLink\" href=\"#\">Delete</a></td></tr>";
+                });
+
+                markup += "</table>";
+
+                $('#currentParts').html(markup);
+                $('#currentParts').html(markup);
+            });
+
+    };
+
+    // initializes everything. called once at page load
+    var initialize = function () {
+        this.initializeDeleteLink();
+        this.initializeCreateNewLink();
+        this.initializeToggleDiscontinuedLink();
+        this.loadCurrentParts();
+    };
 
     return {
+        initialize: initialize,
         initializeDeleteLink: initializeDeleteLink,
-        initializeCreateNewLink: initializeCreateNewLink
+        initializeCreateNewLink: initializeCreateNewLink,
+        initializeToggleDiscontinuedLink: initializeToggleDiscontinuedLink,
+        loadCurrentParts: loadCurrentParts,
+        loadDiscontinuedParts: loadDiscontinuedParts
     };
 };
--- receivingAppJS/Scripts/receivingApp/vendor.js
+++ receivingAppJS/Scripts/receivingApp/vendor.js
@@ -1,6 +1,6 @@
 window.receivingApp.vendor = function () {
     // initializes all of the delete links in the current vendors list and wires up their AJAX post
-    var initializeDeleteLink = function() {
+    var initializeDeleteLink = function () {
         $('#currentVendors').on('click', '.deleteVendorLink', function (event) {
             event.preventDefault();
 
@@ -20,7 +20,90 @@ window.receivingApp.vendor = function () {
         });
     };
 
+    // initializes the create new link to open a popup window
+    var initializeCreateNewLink = function () {
+        $('#createNewVendor').on('click', function () {
+            $('#createNewVendorForm').dialog({
+                resizable: false,
+                width: 500,
+                modal: true,
+                title: "New Vendor",
+                buttons: {
+                    "Create": function () {
+                        var data = $('#createNewVendorForm').serialize();
+                        $.ajax({
+                            type: 'POST',
+                            url: '/Vendor/Create',
+                            data: data,
+                            context: this,
+                            title: "New Vendor",
+                            dataType: "json"
+                        }).done(function (result) {
+                                // Look at all this lovely markup
+                                $('#vendorsList tbody').append('' +
+                                    '<tr data-vendorid="' + result.Id + '"><input type="hidden" name="id" value="' + result.Id + '">' +
+                                    '<td>' + result.Name + '</td>' +
+                                    '<td>' + result.Address1 + '</td>' +
+                                    '<td>' + result.City + '</td>' +
+                                    '<td>' + result.State + '</td>' +
+                                    '<td>' + result.Zip + '</td>' +
+                                    '<td><a class="deleteVendorLink" href="#">Delete</a>' +
+                                    '</form></td>');
+
+                                $(this).dialog("close");
+                                $(this).find('input').val('');
+                            });
+
+                    },
+                    Cancel: function () {
+                        $(this).dialog("close");
+                        $(this).first('input').val('');
+                    }
+                }
+            });
+        });
+    };
+
+    // loads/reloads the current vdndors grid
+    var loadCurrentVendors = function () {
+        $.ajax({
+            url: '/Vendor/CurrentVendors',
+            type: 'GET',
+            context: this,
+            dataType: "json"
+        }).done(function (result) {
+                var markup = '<table class="table" id="vendorsList"><tr><th>Name</th><th>Address 1</th><th>City</th><th>State</th><th>Zip</th><th></th></tr>';
+
+                _.each(result, function (item) {
+                    markup += '<tr data-vendorid="' + item.Id + '">' +
+                        '<input type="hidden" name="id" value="' + item.Id + '" />' +
+                        '<td>' + item.Name + '</td>' +
+                        '<td>' + item.Address1 + '</td>' +
+                        '<td>' + item.City + '</td>' +
+                        '<td>' + item.State + '</td>' +
+                        '<td>' + item.Zip + '</td>' +
+                        '<td><a class="deleteVendorLink" href="#">Delete</a></td>' +
+                        '</tr>';
+                });
+
+                markup += "</table>";
+
+                $('#currentVendors').html(markup);
+            });
+
+
+    };
+
+    var initialize = function () {
+        initializeCreateNewLink();
+        initializeDeleteLink();
+        loadCurrentVendors();
+    };
+
     return {
-        initializeDeleteLink: initializeDeleteLink
+        initialize: initialize,
+        initializeDeleteLink: initializeDeleteLink,
+        initializeCreateNewLink: initializeCreateNewLink,
+        loadCurrentVendors: loadCurrentVendors,
     };
 };
