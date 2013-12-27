d310707781c17d854e477933bec678ade49edf12
Jared Faris <jfaris@gmail.com>
2013-12-27 18:54:17 -0500

    Persistence via local storage

--- receivingAppJS/Scripts/server.js
+++ receivingAppJS/Scripts/server.js
@@ -1,49 +1,83 @@
 (function($) {
-    // parts
-    var parts = [];
-    [
-        "Anchor",
-        "Bottle",
-        "Bigger Hammer",
-        "Cog",
-        "Everburning Torch",
-        "Hammer",
-        "Really Big Hammer",
-        "Screwdriver",
-        "Super Big Hammer",
-        "Unstoppable Hammer"
-    ].forEach(function(item, index) {
-        parts.push({
-            Id: index,
-            Name: item,
-            Weight: index + 1,
-            Discontinued: !!(index % 2)
-        });
-    });
 
-    // vendors
-    var vendors = [];
-    [
-        "Acme",
-        "Bob's Hardware",
-        "CogCo",
-        "DisIsAFakeName",
-        "Emeryville Supply",
-        "Fakestown Indisutrial",
-        "Great Company LTD",
-        "Having trouble coming up with more fake names INC",
-        "IM Nearly Out of Ideas Co",
-        "My Fake Company"
-    ].forEach(function(item, index) {
-        vendors.push({
-            Id: index,
-            Address1: "1" + index + index + " Main Street",
-            City: "Sprintfield",
-            State: "IL",
-            Name: item,
-            Zip: "" + (index) + (index) + index + index + index
-        })
-    });
+    if (!('localStorage' in window && window['localStorage'] !== null)){
+        alert('Seriously, you need a newer browser.')
+    }
+
+    // resets all the models on a page. Use this if the page is loaded for the first time or if someone needs to reset bad data
+    var resetModels = function() {
+        // parts
+        var _parts = [];
+        [
+            "Anchor",
+            "Bottle",
+            "Bigger Hammer",
+            "Cog",
+            "Everburning Torch",
+            "Hammer",
+            "Really Big Hammer",
+            "Screwdriver",
+            "Super Big Hammer",
+            "Unstoppable Hammer"
+        ].forEach(function(item, index) {
+                _parts.push({
+                    Id: index,
+                    Name: item,
+                    Weight: index + 1,
+                    Discontinued: !!(index % 2)
+                });
+            });
+        parts.set(_parts);
+
+
+        // vendors
+        var _vendors = [];
+        [
+            "Acme",
+            "Bob's Hardware",
+            "CogCo",
+            "DisIsAFakeName",
+            "Emeryville Supply",
+            "Fakestown Indisutrial",
+            "Great Company LTD",
+            "Having trouble coming up with more fake names INC",
+            "IM Nearly Out of Ideas Co",
+            "My Fake Company"
+        ].forEach(function(item, index) {
+                _vendors.push({
+                    Id: index,
+                    Address1: "1" + index + index + " Main Street",
+                    City: "Sprintfield",
+                    State: "IL",
+                    Name: item,
+                    Zip: "" + (index) + (index) + index + index + index
+                })
+            });
+        vendors.set(_vendors);
+    }
+
+    // cheater helpers for local storage
+    var parts = {
+        get: function() {
+            return JSON.parse(localStorage.getItem("parts"))
+        },
+        set: function(newParts) {
+            localStorage.setItem("parts", JSON.stringify(newParts))
+        }
+    };
+    var vendors = {
+        get: function() {
+            return JSON.parse(localStorage.getItem("vendors"))
+        },
+        set: function(newVendors) {
+            localStorage.setItem("vendors", JSON.stringify(newVendors))
+        }
+    };
+
+    if (parts.get().length == 0  || vendors.get().length == 0) {
+        alert("Resetting the data because an array is empty!")
+        resetModels()
+    }
 
     var dataToObject = function(dataString) {
         var result = {};
@@ -59,7 +93,7 @@
     $.mockjax({
         url: '/Part/DiscontinuedParts',
         response: function(settings) {
-            this.responseText = _.filter(parts, function(part) {
+            this.responseText = _.filter(parts.get(), function(part) {
                 return part.Discontinued;
             })
         }
@@ -68,7 +102,7 @@
     $.mockjax({
         url: '/Part/CurrentParts',
         response: function(settings) {
-            this.responseText = _.filter(parts, function(part) {
+            this.responseText = _.filter(parts.get(), function(part) {
                 return !part.Discontinued;
             })
         }
@@ -79,51 +113,67 @@
         response: function(settings) {
             var id = settings.data.Id;
 
-            var partToDelete = _.find(parts, function(item) {
+            var allParts = parts.get();
+
+            var partToDelete = _.find(allParts, function(item) {
                 return item.Id == id;
             });
 
             partToDelete.Discontinued = true;
+
+            parts.set(allParts);
         }
     });
 
     $.mockjax({
         url: '/Part/Create',
         response: function(settings) {
+            var allParts = parts.get();
+
             var newPart = dataToObject(settings.data);
-            var maxId = _.max(parts, function(part) { return part.Id });
+            var maxId = _.max(allParts, function(part) { return part.Id });
             newPart.Id = maxId + 1;
             newPart.Discontinued = false;
-            parts.push(newPart);
+            allParts.push(newPart);
             this.responseText = newPart;
+
+            parts.set(allParts);
         }
     });
 
     $.mockjax({
         url: '/Vendor/CurrentVendors',
         response: function(settings) {
-            this.responseText = vendors;
+            this.responseText = vendors.get();
         }
     });
 
     $.mockjax({
         url: '/Vendor/Delete',
         response: function(settings) {
+            var allVendors = vendors.get();
+
             var id = settings.data.Id;
-            vendors = _.reject(vendors, function(item) {
+            allVendors = _.reject(allVendors, function(item) {
                 return item.Id == id;
             });
+
+            vendors.set(allVendors);
         }
     });
 
     $.mockjax({
         url: '/Vendor/Create',
         response: function(settings) {
+            var allVendors = vendors.get();
+
             var newVendor = dataToObject(settings.data);
-            var maxId = _.max(vendors, function(vendor) { return vendor.Id });
+            var maxId = _.max(allVendors, function(vendor) { return vendor.Id });
             newVendor.Id = maxId + 1;
-            vendors.push(newVendor);
+            allVendors.push(newVendor);
             this.responseText = newVendor;
+
+            vendors.set(allVendors);
         }
     });
 })($);
